2c6223e9c4528f6c9ec7532abefcea143ac9a67d
Sean <zhuqingyun@outlook.com>
2022-01-04 14:59:31 -0700

    added some nodejs code

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-const userName = "Someone";
+const http = require('http');
 
+const server = http.createServer();
+
+server.listen(3000);
+
+// amazon.com => Send request to Amazon's server
+// amazon.com:443 
