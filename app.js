d010f242c7f8deffe91ba93041364cc5f64a91d2
Sean <zhuqingyun@outlook.com>
2022-01-04 22:38:27 -0700

    minor update

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get('/currenttime', function (req, res) {
 
 
 app.get('/', function (req, res) {
-    res.send('<form action="/store-user" method="POST"><label>Your Name</label><input type="text" name="username"><button>Submit</button></form>');
+    res.send('<form action="/store-user" method="POST"><label>Your Name :</label><input type="text" name="username"><button>Submit</button></form>');
 });  // localhost:3000/
 
 
